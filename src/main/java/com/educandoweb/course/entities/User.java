a82027c595924a3dc976b8ac8bc59fa5a6858f7d
RAPHAEL DE LEMOS PIRES <raphaellpires@gmail.com>
2026-04-16 08:22:33 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
     public User() {
     }
 
-    public User(long id, String name, String email, String phone, String password) {
+    public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -28,11 +28,18 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public long getId() {
+    public User(String name, String email, String phone, String password) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
