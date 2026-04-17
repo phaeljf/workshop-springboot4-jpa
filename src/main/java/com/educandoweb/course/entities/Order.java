74011510b51a97c5aac4880050522865f30c4b58
RAPHAEL DE LEMOS PIRES <raphaellpires@gmail.com>
2026-04-17 08:51:36 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -19,6 +20,7 @@ public class Order implements Serializable {
     @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -27,8 +29,9 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Instant moment, User client) {
+    public Order(Instant moment, OrderStatus orderStatus, User client) {
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -39,6 +42,16 @@ public class Order implements Serializable {
     public Instant getMoment() { return moment; }
 
     public void setMoment(Instant moment) { this.moment = moment; }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 
     public User getClient() {return client; }
 
